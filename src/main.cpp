639799b9c2fe59cd37d76a5d7a9eb03bc127ee51
quigley-c <carson.ll.quigley@gmail.com>
2022-02-28 15:06:04 -0500

    ros commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 #include <turtlesim/Pose.h>
 #include <geometry_msgs/Twist.h>
 
-turtlesim::PoseConstPtr pose;
+turtlesim::PoseConstPtr pose_pt;
 turtlesim::Pose goal;
 
+void poseCallback(const turtlesim::PoseConstPrt& pose)	{
+	pose_pt = pose;
+}
+
 int main()	{
-	vel_msg = Twist();
+	ros::init();
+	ros::NodeHandle nh;
+	ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
+	ros::Publisher twist_pub = nh.advertise<geometry_msgs:Twist>("turtle1/cmd_vel", 1);
 
 	return 0;
 }
